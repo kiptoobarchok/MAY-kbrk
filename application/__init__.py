291437145303794107dad27950b236eb7209c01c
kiptoobarchok <calebkiptoo9090@gmail.com>
2025-05-16 12:18:59 +0300

    deployment update

--- application/__init__.py
+++ application/__init__.py
@@ -22,11 +22,11 @@ if not secret_key:
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///may_kbrk.db'
 
 # Flask mail configuration
-app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
-app.config['MAIL_USE_TLS']=  os.getenv('MAIL_USE_TLS') == 'True'
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+# app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+# app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+# app.config['MAIL_USE_TLS']=  os.getenv('MAIL_USE_TLS') == 'True'
+# app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+# app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
 mail=Mail(app)
 
